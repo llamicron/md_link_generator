81974eb92dc966baa073d3fa26c3dfce319ec67b
Luke Sweeney <luke@thesweeneys.org>
2017-06-07 10:40:32 -0500

    Reduces down to 100 characters

--- md_link_gen.cr
+++ md_link_gen.cr
@@ -1,1 +1,4 @@
-Dir.glob("**/*").select { |file| file.ends_with?(".md") }.each { |file| puts "[#{file.chomp(".md")}]" + "(#{file})" + "\n\n" }
+# Prettier version
+# Dir.glob("**/*").select { |file| file.ends_with?(".md") }.each { |file| puts "[#{file.chomp(".md")}]" + "(#{file})" + "\n\n" }
+# Shorter version. I know it's against style guides but I need to save those characters
+Dir.glob("**/*").select{|f|f.ends_with?(".md")}.each{|f|puts "[#{f.chomp(".md")}]"+"(#{f})"+"\n\n"}
